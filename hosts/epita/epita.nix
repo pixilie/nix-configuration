e5c87e92b84dbeeb288380478be05c8d66f2fc75
Kristen Couty <kristen.couty@gmail.com>
2025-01-10 14:09:00 +0100

    feat: epita computer config draft

---
+++ hosts/epita/epita.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/helix.nix
+    ../../modules/home-manager/sh.nix
+    ../../modules/home-manager/fonts.nix
+  ];
+
+  # General informations
+  home.username = "kristen-epita";
+  home.homeDirectory = "/home/kristen-epita";
+  home.stateVersion = "24.11";
+
+  # Packages
+  home.packages = with pkgs; [ tidal-hifi ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
