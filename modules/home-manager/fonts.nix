e26e93ac5c7ea09c42cc3ef223931621e7e64824
Kristen Couty <kristen.couty@gmail.com>
2025-11-13 22:34:36 +0100

    fix: rofi fonts

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     noto-fonts
     noto-fonts-emoji
-    nerd-fonts.jetbrains-mono
+    # nerd-fonts.jetbrains-mono
     nerd-fonts.caskaydia-cove
     nerd-fonts.caskaydia-mono
     font-awesome
