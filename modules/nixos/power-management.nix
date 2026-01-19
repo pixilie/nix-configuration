cd077d8272b5cafcfd92141cc0ac054b4d2c3f0c
Kristen Couty <kristen.couty@gmail.com>
2026-01-19 18:07:26 +0200

    fix: power management tool overlap

--- modules/nixos/power-management.nix
+++ modules/nixos/power-management.nix
@@ -1,9 +1,10 @@
 { ... }:
 
 {
+  services.power-profiles-daemon.enable = false;
+
   services.auto-cpufreq = {
     enable = true;
-
     settings = {
       battery = {
         governor = "powersave";
@@ -13,22 +14,6 @@
         governor = "performance";
         turbo = "auto";
       };
-    };
-  };
-
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "keep";
-      CPU_SCALING_GOVERNOR_ON_BAT = "keep";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "keep";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "keep";
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 100;
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 }
