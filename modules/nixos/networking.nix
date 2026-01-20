7b0af8c4ee20aad828c75bddd90f23b0dfe3f5a9
Kristen Couty <kristen.couty@gmail.com>
2026-01-20 14:46:56 +0200

    fix: remove unsued open ports

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -6,17 +6,6 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-
-      allowedTCPPortRanges = [{
-        from = 1714;
-        to = 1764;
-      }];
-
-      allowedUDPPortRanges = [{
-        from = 1714;
-        to = 1764;
-      }];
-
       allowedUDPPorts = [ 8080 ];
       allowedTCPPorts = [ 8080 ];
     };
