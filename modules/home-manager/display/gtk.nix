8016fcd7ad9124be549983c9f2a63d940afe80ab
Kristen Couty <kristen.couty@gmail.com>
2026-01-21 12:51:36 +0200

    feat: cursor style

--- modules/home-manager/display/gtk.nix
+++ modules/home-manager/display/gtk.nix
@@ -14,4 +14,12 @@
     platformTheme.name = "gtk";
     style.name = "gtk2";
   };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
 }
