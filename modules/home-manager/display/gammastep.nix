ba0bff611f61b213df4f49039c349e30e2a2fe20
Kristen Couty <kristen.couty@gmail.com>
2026-01-23 20:15:42 +0200

    fix: gammastep config

--- modules/home-manager/display/gammastep.nix
+++ modules/home-manager/display/gammastep.nix
@@ -4,9 +4,12 @@
   services.gammastep = {
     enable = true;
     provider = "geoclue2";
+
     temperature = {
       day = 5700;
       night = 3200;
     };
+
+    settings = { general.fade = "1"; };
   };
 }
