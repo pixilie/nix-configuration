6ef8400329923833b2c3d27b754c282347f3cb48
Kristen Couty <kristen.couty@gmail.com>
2026-04-10 21:33:24 +0300

    fix: xdg to stream on discord

--- modules/features/desktop/niri/niri.nix
+++ modules/features/desktop/niri/niri.nix
@@ -20,7 +20,10 @@
 
     xdg.portal = {
       enable = true;
-      config.common.default = "*";
+      xdgOpenUsePortal = true;
+      extraPortals =
+        [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-gnome ];
+      config = { common.default = [ "gnome" "*" ]; };
     };
   };
 
